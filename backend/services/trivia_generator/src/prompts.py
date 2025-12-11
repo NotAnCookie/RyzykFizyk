81e5a33077cd7020b264bbbe6749f5f89902675c
NotAnCookie <160712740+NotAnCookie@users.noreply.github.com>
2025-12-11 19:25:41 +0100

    a bit better prompts for 5-nano

--- backend/services/trivia_generator/src/prompts.py
+++ backend/services/trivia_generator/src/prompts.py
@@ -1,11 +1,13 @@
 TRIVIA_PROMPT_TEMPLATE = {
     "pl": (
-        "Podaj jedną krótką, ciekawą ciekawostkę związaną z pytaniem: '{topic}'. Nie odpowiadaj na pytanie. "
+        #"Podaj jedną krótką, ciekawą ciekawostkę związaną z pytaniem: '{topic}'. Nie odpowiadaj na pytanie. "
         # "Zachowaj styl informacyjny. Jeśli to możliwe, dodaj źródło w nawiasie (np. Wikipedia)."
+        "Opowiedz zabawną lub nietypową ciekawostkę inspirowaną tematem tego pytania. Nie odpowiadaj na pytanie i nie używaj liczb oraz ogranicz się do jednego krótkiego zdania. {topic}"
     ),
 
     "en": (
-        "Provide one short and interesting trivia related to the question: '{topic}'. Don't answer the question."
+        #"Provide one short and interesting trivia related to the question: '{topic}'. Don't answer the question."
         # "Keep an informative tone. If possible, include a source in parentheses (e.g., Wikipedia)."
+        "Tell a funny or unusual fact inspired by the topic of this question. Do not answer the question, do not use numbers, and keep it to a single short sentence. {topic}"
     ),
 }
