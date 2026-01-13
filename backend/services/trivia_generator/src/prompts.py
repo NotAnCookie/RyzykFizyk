1a016057046e8cc8894b49af1a004a66f08ae66a
NotAnCookie <160712740+NotAnCookie@users.noreply.github.com>
2026-01-13 12:13:40 +0100

    better trivia prompt

--- backend/services/trivia_generator/src/prompts.py
+++ backend/services/trivia_generator/src/prompts.py
@@ -2,12 +2,24 @@ TRIVIA_PROMPT_TEMPLATE = {
     "pl": (
         #"Podaj jedną krótką, ciekawą ciekawostkę związaną z pytaniem: '{topic}'. Nie odpowiadaj na pytanie. "
         # "Zachowaj styl informacyjny. Jeśli to możliwe, dodaj źródło w nawiasie (np. Wikipedia)."
-        "Opowiedz zabawną lub nietypową ciekawostkę inspirowaną tematem tego pytania. Nie odpowiadaj na pytanie i nie używaj liczb oraz ogranicz się do jednego krótkiego zdania. {topic}"
+        #"Opowiedz zabawną lub nietypową ciekawostkę inspirowaną tematem tego pytania. Nie odpowiadaj na pytanie i nie używaj liczb oraz ogranicz się do jednego krótkiego zdania. {topic}"
+        "Podaj jedną krótką, mało znaną ciekawostkę związaną z tematem pytania."
+        "Nie odpowiadaj bezpośrednio na pytanie."
+        "Unikaj najbardziej znanych i oczywistych faktów; wybierz coś drugoplanowego, rzadziej wspominanego lub zaskakującego."
+        "Ciekawostka powinna być oparta na rzeczywistym fakcie i podana w lekkim, lekko żartobliwym stylu."
+        "Ogranicz się do jednego zdania, nie używaj liczb. {topic}"
+
     ),
 
     "en": (
         #"Provide one short and interesting trivia related to the question: '{topic}'. Don't answer the question."
         # "Keep an informative tone. If possible, include a source in parentheses (e.g., Wikipedia)."
-        "Tell a funny or unusual fact inspired by the topic of this question. Do not answer the question, do not use numbers, and keep it to a single short sentence. {topic}"
+        #"Tell a funny or unusual fact inspired by the topic of this question. Do not answer the question, do not use numbers, and keep it to a single short sentence. {topic}"
+        "Provide one short, lesser-known trivia related to the topic of the question.."
+        "Do not answer the question directly."
+        "Avoid the most common or headline facts; choose a secondary, overlooked, or surprising detail."
+        "The trivia should be based on a real fact, written in a light and playful tone with a subtle touch of humor."
+        "Keep it to one sentence and do not use numbers. {topic}"
+
     ),
 }
